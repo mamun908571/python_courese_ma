cb223e9ea986c3aee8fc0597d2e98822b610f40f
mamun908571 <150835285+mamun908571@users.noreply.github.com>
2023-11-23 00:49:38 +0600

    Add files via upload

---
+++ basic/dictionary.py
@@ -0,0 +1,21 @@
+# car = {"model":"ml","color":"red","price":100}
+# print(car['model'])
+
+# car = {"model":"ml","color":"red","price":100}
+# print(car.keys())
+
+# car = {"model":"ml","color":"red","price":100}
+# print(car.values())
+
+# car = {"model":"ml","color":"red","price":100}
+# for i in car:
+#     print(i)
+
+
+# car = {"model":"ml","color":"red","price":100}
+# for i in car.values():
+#     print(i)
+
+car = {"model":"ml","color":"red","price":100}
+for i,j in car.items():
+    print(i,j)
